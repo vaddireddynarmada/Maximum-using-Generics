ae90c7e9c04c4f75ab945637b6e1723475e9670b
unknown <narmada.vaddireddy97@gmail.com>
2023-03-07 19:44:54 +0530

    Refactor: Code to print Maximum Element of Generic by variable

--- MaxValue.java
+++ MaxValue.java
@@ -1,20 +1,31 @@
 public class MaxValue<T extends Comparable<T>> {
-    T num1, num2, num3;
+    T num1;
+    T num2;
+    T num3;
 
-    public <T extends Comparable<T>> T maximumGeneric(T num1, T num2, T num3) {
+    public MaxValue(T num1, T num2, T num3) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+
+    public static <T extends Comparable<T>> T maximumGenerict(T num1, T num2, T num3) {
         T maximumValue = num1;
         if (num2.compareTo(maximumValue) > 0)
             maximumValue = num2;
         if (num3.compareTo(maximumValue) > 0)
             maximumValue = num3;
-        System.out.println(num1 + " " + num2 + " " + num3);
+        System.out.println(num1+" "+num2+" "+num3);
         return maximumValue;
     }
 
     public static void main(String[] args) {
-        MaxValue maximum = new MaxValue();
-        System.out.println("Maximum Integer value : " + maximum.maximumGeneric(10, 78, 34));
-        System.out.println("Maximum Float value : " + maximum.maximumGeneric(10.3f, 20.9f, 56.2f));
-        System.out.println("Maximum String value : " + maximum.maximumGeneric("xyz", "ew", "re"));
+        Integer intValue = maximumGenerict(10, 34, 78);
+        Float floatValue = maximumGenerict(10.3f, 20.9f, 56.2f);
+        String stringValue = maximumGenerict("xyz", "ew", "re");
+        System.out.println("Maximum Integer value : " + intValue);
+        System.out.println("Maximum Float value : " + floatValue);
+        System.out.println("Maximum String value : " + stringValue);
+
     }
 }
