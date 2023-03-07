0a0b09a8bfd149c19fedaf0bfc449e9b25e77749
unknown <narmada.vaddireddy97@gmail.com>
2023-03-07 19:27:40 +0530

    Refactor: Code to print Maximum Element of generic type

--- MaxValue.java
+++ MaxValue.java
@@ -1,16 +1,20 @@
-public class MaxValue {
-    public static String maximumString(String a, String b, String c) {
-        String maximumValue = a;
-        if (b.compareTo(maximumValue) > 0)
-            maximumValue = b;
-        if (c.compareTo(maximumValue) > 0)
-            maximumValue = c;
-        System.out.println(a+" "+b+" "+c);
+public class MaxValue<T extends Comparable<T>> {
+    T num1, num2, num3;
+
+    public <T extends Comparable<T>> T maximumGeneric(T num1, T num2, T num3) {
+        T maximumValue = num1;
+        if (num2.compareTo(maximumValue) > 0)
+            maximumValue = num2;
+        if (num3.compareTo(maximumValue) > 0)
+            maximumValue = num3;
+        System.out.println(num1 + " " + num2 + " " + num3);
         return maximumValue;
     }
 
     public static void main(String[] args) {
-        MaxValue maxValue = new MaxValue();
-        System.out.println("Maximum value of String : "+maxValue.maximumString("apple", "peach", "banana"));
+        MaxValue maximum = new MaxValue();
+        System.out.println("Maximum Integer value : " + maximum.maximumGeneric(10, 78, 34));
+        System.out.println("Maximum Float value : " + maximum.maximumGeneric(10.3f, 20.9f, 56.2f));
+        System.out.println("Maximum String value : " + maximum.maximumGeneric("xyz", "ew", "re"));
     }
 }
