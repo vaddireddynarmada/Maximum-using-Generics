81f37d729d2a8e1a71b1cd0e3269590e5029293c
unknown <narmada.vaddireddy97@gmail.com>
2023-03-07 19:12:10 +0530

    Added: Code to print Maximum Element of Float

--- MaxValue.java
+++ MaxValue.java
@@ -1,14 +1,15 @@
 public class MaxValue {
-    public Integer maximumInteger(Integer a, Integer b, Integer c) {
-        Integer maximumValue = a;
+    public Float maximumFloat(Float a, Float b, Float c) {
+        Float maximumValue = a;
         if (b.compareTo(maximumValue) > 0)
             maximumValue = b;
         if (c.compareTo(maximumValue) > 0)
             maximumValue = c;
         return maximumValue;
+
     }
     public static void main(String[] args){
         MaxValue maxValue=new MaxValue();
-        System.out.println("Maximum Value of Integer : "+maxValue.maximumInteger(12,14,23));
+        System.out.println(maxValue.maximumFloat(20.2F,15.14F,18.23F));
     }
 }
