ede37f69ad7ced2bf06db4043ca90e24315c9d28
unknown <narmada.vaddireddy97@gmail.com>
2023-03-07 21:06:01 +0530

    Added: Code to print the maximum element by printMax

--- MaxValue.java
+++ MaxValue.java
@@ -1,30 +1,30 @@
 import java.util.Arrays;
 
 public class MaxValue<T extends Comparable<T>> {
-    T[] maxValue;
-
-    public MaxValue(T[] maxValue) {
-        this.maxValue = maxValue;
-
-    }
-    public static <T extends Comparable<T>> T[] maximum(T... maxValue) {
-        System.out.println("Given elements ");
-        for (T element : maxValue) {
-            System.out.print(element + " ");
+    public static <T extends Comparable<T>> T maximum(T... number) {
+        T maxValue = number[0];
+        for (int i = 1; i < number.length; i++) {
+            if (number[i].compareTo(maxValue) > 0) {
+                maxValue = number[i];
+            }
+        }
+        System.out.println("given values are : ");
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i]+" ");
         }
         System.out.println();
-        System.out.println("After Sorting : ");
-        Arrays.sort(maxValue);
-        for (T element : maxValue) {
-            System.out.print(element + " ");
-        }
-        System.out.println();
+        printMax(maxValue);
         return maxValue;
     }
 
+    public static <T> void printMax(T max) {
+        System.out.println("Maximum Value is : " + max);
+    }
+
+    // main method
     public static void main(String[] args) {
-        maximum(23, 78, 36,45,23,2,5,78);
-        maximum("xyz", "dij", "jsd","drg","wdd");
-        maximum(23f, 9f, 4.3f,56.3f,34.2f);
+        maximum(23, 78, 36, 45, 23, 2, 5, 78);
+        maximum("xyz", "dij", "jsd", "drg", "wdd");
+        maximum(23f, 9f, 4.3f, 56.3f, 34.2f);
     }
 }
