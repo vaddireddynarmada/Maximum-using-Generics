6e8f0e74a1c6778aa79a9fabb4ec26b18dd46d32
unknown <narmada.vaddireddy97@gmail.com>
2023-03-07 19:16:42 +0530

    Added: Code to print Maximum Element of String

--- MaxValue.java
+++ MaxValue.java
@@ -1,15 +1,16 @@
 public class MaxValue {
-    public Float maximumFloat(Float a, Float b, Float c) {
-        Float maximumValue = a;
+    public static String maximumString(String a, String b, String c) {
+        String maximumValue = a;
         if (b.compareTo(maximumValue) > 0)
             maximumValue = b;
         if (c.compareTo(maximumValue) > 0)
             maximumValue = c;
+        System.out.println(a+" "+b+" "+c);
         return maximumValue;
-
     }
-    public static void main(String[] args){
-        MaxValue maxValue=new MaxValue();
-        System.out.println(maxValue.maximumFloat(20.2F,15.14F,18.23F));
+
+    public static void main(String[] args) {
+        MaxValue maxValue = new MaxValue();
+        System.out.println("Maximum value of String : "+maxValue.maximumString("apple", "peach", "banana"));
     }
 }
